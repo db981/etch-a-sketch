d78a2a3f503ca50b207879920f7de80fcd80710e
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2022-11-02 16:20:28 -0400

    Get dimension from user, colour cells when hovered

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-function generateGrid(dimensions = 20){
+let cells = [];
+let userDimensions;
+while(!userDimensions){
+    let userPrompt = prompt("Enter grid dimensions (max 100)");
+    if(userPrompt >= 1 && userPrompt <= 100){
+        generateGrid(userPrompt);
+        break;
+    }
+}
+
+cells.forEach(cell => cell.addEventListener('mouseover', cellHovered));
+
+function generateGrid(dimensions = 100){
     const grid = document.querySelector(".grid");
     const squareLength = grid.offsetHeight/dimensions;
-    console.log(squareLength);
     
     for(let x = 0; x < dimensions**2; x++){
         const cell = document.createElement("div");
         cell.classList.add('cell');
         cell.style.height = `${squareLength}px`;
         cell.style.width = `${squareLength}px`;
-        cell.style.border = '1px solid black';
+        cell.style.outline = '1px solid black';
         grid.appendChild(cell);
+        cells.push(cell);
     }
 }
-generateGrid();
+
+function cellHovered(event){
+    event.target.style.backgroundColor = "black";
+}
